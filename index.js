2fd2b4afe91a96da7916015fe73b1639fc0fe277
Luaranet <luaranet08@gmail.com>
2024-06-27 15:17:32 -0400

    commit

--- index.js
+++ index.js
@@ -1,61 +1,125 @@
-require ("dotenv").config();
+require("dotenv").config();
 const conn = require("./db/conn");
 const express = require("express");
 const exphbs = require("express-handlebars");
 
-const Usuario =  require("./models/Usuario");
+const Usuario = require("./models/Usuario");
+const Jogo = require("./models/Jogo")
 
-  const app = express();
+const app = express();
 
 app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
 
 
-  app.use(
-    express.urlencoded({
-      extended: true,
-    })
-  )
-
-  app.use(express.json());
-
-  app.get("/usuarios/novo", (req,res) => {
-    res.render("formUsuario")
+app.use(
+  express.urlencoded({
+    extended: true,
   })
+)
 
-  app.get("/usuarios", async (req, res) => {
-    const usuarios = await Usuario.findAll({
-      raw:true });
-      res.render("usuarios",{usuario});
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.render("home")
+})
+
+//usuarios
+app.get("/usuarios", async (req, res) => {
+  const usuarios = await Usuario.findAll({
+    raw: true
+  });
+  res.render("usuarios", { usuarios });
+});
+
+
+app.get("/usuarios/novo", (req, res) => {
+  res.render("formUsuario")
+})
+
+app.post("/usuarios/novo", async (req, res) => {
+  const dadosUsuario = {
+    nickname: req.body.nickname,
+    nome: req.body.nome,
+  };
+
+  const usuario = await Usuario.create(dadosUsuario);
+  res.send("Usuário inserido: " + usuario.id)
+});
+
+app.get("/usuarios/:id/update", async (req, res) => {
+  const id = parseInt(req.params.id);
+  const usuario = await Usuario.findByPk(id, { raw: true });
+  res.render("formUsuario", { usuario })
+})
+
+app.post("/usuarios/:id/update", async (req, res) => {
+  const id = parseInt(req.params.id);
+  const dadosUsuario = {
+    nickname: req.body.nickname,
+    nome: req.body.nome,
+  };
+  const retorno = await Usuario.update(dadosUsuario, {
+    where: { id: id },
   });
 
-  app.post("/usuarios/novo" , async(req,res) => {
-    const nickname = req.body.nickname;
-    const nome = req.body.nome;
+  if (retorno > 0) {
+    res.redirect("/usuarios");
+  }
+  else {
+    res.send("foi de vasco");
+  }
+})
 
-    const dadosUsuario = {
-      nickname,
-      nome,
-    };
+//delete
+app.post("/usuarios/:id/delete", async (req, res) => {
+  const id = parseInt(req.params.id);
+  const retorno = await Usuario.destroy({ where: { id: id } });
 
-    const usuario = await Usuario.create(dadosUsuario);
-    res.send("Usuário inserido: " + usuario.id)
+  if (retorno > 0) {
+    res.redirect("/usuarios");
+  } else {
+    res.send("Erro ao excluir usuário");
+  }
+});
+
+
+//jogo
+app.get("/jogos", async (req, res) => {
+  const jogos = await Jogo.findAll({
+    raw: true
   });
+  res.render("jogos", { jogos });
+});
 
-  app.get("/usuarios/:id/update", async (req, res) => {
-    const id = parseInt(req.params.id);
-    const usuario = await Usuario.findByPk(id, {raw: true});
+app.get("/jogos/novo", (req, res) => {
+  res.render("formJogo")
+})
 
-    res.render("formUsuario", { usuario });
-    // const usuario = Usuario.findOne({
-    // where:{ id:id },
-    // raw: true,
-    // });
-  });
 
-  app.listen(8000, () => {
-    console.log("o servidor está rodando na porta 8000")
-  });
+
+app.get("/jogos/:id/update", async (req, res) => {
+  const id = parseInt(req.params.id);
+  const jogo = await Jogo.findByPk(id, { raw: true });
+
+  res.render("formJogo", { jogo });
+});
+
+app.post("/jogos/novo", async (req, res) => {
+  const dadosJogo = {
+    titulo: req.body.titulo,
+    descricao: req.body.descricao,
+    preco: req.body.preco,
+  };
+
+  const jogo = await Jogo.create(dadosJogo);
+  res.send("Jogo inserido: " + jogo.id)
+});
+
+
+app.listen(8000, () => {
+  console.log("o servidor está rodando na porta 8000")
+});
 
 conn
   .sync()
